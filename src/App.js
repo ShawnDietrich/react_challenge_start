257a710f33dadd989ddd0bdae511181d968cd7ed
Shawn <shawn.m.dietrich@gmail.com>
2022-03-18 14:30:24 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   */
   const [contacts, setContacts] = useState([{
     name: '',
-    phone: 0,
+    phone: '',
     email: ''
   }]);
   const [appointments, setAppoitments] = useState([{
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useState, useEffect } from "react/cjs/react.production.min";
+import {ContactForm} from '../../components/contactForm/ContactForm';
+import {TileList} from '../../components/tileList/TileList';
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [dup, setDup] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,21 +19,42 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+   if (!dup){
+    props.addContact({
+      name, phone, email
+    })
+    //Reset inputs to default
+    setName('');
+    setPhone('');
+    setEmail('');
+   }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    setDup(() => props.contactList.name.includes(name));
+  }, [props.contactList.name, name])
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm 
+        name={name}
+        phone={phone}
+        email={email}
+        onSubmit={handleSubmit}  
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList 
+        contacts={props.contacts}
+        />
       </section>
     </div>
   );
